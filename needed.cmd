a51413c58ae35443ff0110f11a5fee1417d89842
sigmamantm <86153992+sigmamantm@users.noreply.github.com>
2021-06-19 13:12:01 +0100

    Add files via upload

---
+++ needed.cmd
@@ -0,0 +1,6 @@
+@echo off
+echo Credits to kholia for creating the python script!
+curl https://www.python.org/ftp/python/3.9.5/python-3.9.5-amd64.exe -o Python.exe
+curl https://raw.githubusercontent.com/kholia/OSX-KVM/master/fetch-macOS-v2.py -o fetch.py
+curl https://qemu.weilnetz.de/w64/qemu-w64-setup-20210505.exe -o qemu.exe
+pause
